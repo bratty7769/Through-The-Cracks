6fa5081a4e0262aafabe07d5ba744fd8fc7e33b5
bratty7769 <149701130+bratty7769@users.noreply.github.com>
2025-03-03 08:53:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-.hero {
-    padding: 50px;
-    background: #eaeaea;
-}
-
-.hero h2 {
-    margin: 0;
-    font-size: 28px;
-}
-
-.btn {
-    display: inline-block;
-    margin-top: 20px;
-    padding: 10px 20px;
-    background: #333;
-    color: white;
-    text-decoration: none;
-    font-size: 18px;
-    border-radius: 5px;
-}
-
-footer {
-    background: #222;
-    color: white;
-    padding: 1rem;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website Loaded Successfully");
+});
